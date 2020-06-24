49006d3746baf233539ecc4be72a7d622a6dd23c
Valera <gavrilow777@gmail.com>
2020-06-24 10:24:47 +0300

    research

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { MainComponent } from './main/main.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
   { path: 'news', loadChildren: async () => (await import('./news/news.module')).NewsModule },
-  { path: 'contacts', loadChildren: async () => (await import('./contacts/contacts.module')).ContactsModule },
+  // { path: 'contacts', loadChildren: () => import('./contacts/contacts.module').then(m => m.ContactsModule) },
+  { path: 'contacts', component: ContactsComponent },
   { path: 'about-us', loadChildren: async () => (await import('./about-us/about-us.module')).AboutUsModule },
   { path: '**', component: NotFoundComponent },  // Wildcard route for a 404 page
 ];
